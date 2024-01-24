617748d5fc2a84c0b95bd8fa514f8fdf39b4bcac
AhmetAMA <33016@ma-web.nl>
2024-01-24 23:57:51 +0100

    hamburger menu

--- code/js/script.js
+++ code/js/script.js
@@ -1,3 +1,20 @@
+/*  Hamburger Menu
+---------------------------------------------------------------------------------------------------- */
+function hamburgerMenu() {
+    
+    var x = document.getElementById("myLinks");
+    
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+/*  the rest
+---------------------------------------------------------------------------------------------------- */
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -48,3 +65,41 @@ function showSlides(n) {
 //   slides[slideIndex-1].style.display = "block";
 //   setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
+
+
+// function myFunction(imgs) {
+//     // Get the expanded image
+//     var expandImg = document.getElementById("expandedImg");
+//     // Get the image text
+//     var imgText = document.getElementById("imgtext");
+//     // Use the same src in the expanded image as the image being clicked on from the grid
+//     expandImg.src = imgs.src;
+//     // Use the value of the alt attribute of the clickable image as text inside the expanded image
+//     imgText.innerHTML = imgs.alt;
+//     // Show the container element (hidden with CSS)
+//     // expandImg.parentElement.style.display = "block";
+// }
+
+
+
+
+
+
+function myFunction(element) {
+    var expandImg = document.getElementById("expandedImg");
+    var expandVideo = document.getElementById("expandedVideo");
+    var imgText = document.getElementById("imgtext");
+
+    if (element.tagName === "IMG") {
+        expandImg.src = element.src;
+        expandImg.style.display = "block";
+        expandVideo.style.display = "none";
+        imgText.innerHTML = element.alt;
+    } else if (element.classList.contains("video-thumbnail")) {
+        var videoPath = element.getAttribute("data-video");
+        expandVideo.src = videoPath;
+        expandVideo.style.display = "block";
+        expandImg.style.display = "none";
+        imgText.innerHTML = "Video";
+    }
+}
